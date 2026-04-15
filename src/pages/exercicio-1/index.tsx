1f4534bbb0cf8ce2b5c1d6ee28abf46299e3f56d
lagrimanteDev <hlagrimante@gmail.com>
2026-04-14 21:55:41 -0300

    feat:simplificacao-exercicio1

--- src/pages/exercicio-1/index.tsx
+++ src/pages/exercicio-1/index.tsx
@@ -1,19 +1,18 @@
 import { useState } from "react"
+import { text } from "stream/consumers"
 
-let temaDefault = ""
 
 export default function Ex01() {
 
     const [tema, setTema] = useState<boolean>(false)
 
-    { tema ? temaDefault = `bg-zinc-100` : temaDefault = `bg-black` }
-
     return (
         <>
-            <div className={`flex flex-col justify-center items-center min-h-screen gap-20 ${temaDefault}`}>
+            <div className={`flex flex-col justify-center items-center min-h-screen gap-20
+                ${tema ? "bg-gray-300" : "bg-black"}`}>
 
-                <h1>Exercicio 01</h1>
-                <button className="flex justify-center items-center cursor-pointer" onClick={() => setTema(!tema)}>Trocar Tema</button>
+                <h1 className={`${tema ? "text-black" : "text-zinc-100"}`}>Exercicio 01</h1>
+                s<button className={`flex justify-center items-center cursor-pointer ${tema ? "text-black" : "text-zinc-100"}`} onClick={() => setTema(!tema)}>Trocar Tema</button>
 
             </div>
         </>
